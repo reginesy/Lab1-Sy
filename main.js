853740f42f2bf4ac3b1d9d6f09570b785c2eafcd
Regine Sy <regine.sy@obf.ateneo.edu>
2018-02-09 20:37:21 +0800

    Intial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var navButton = document.querySelector('nav button');
+
+navButton.addEventListener('click', function() {
+  let expanded = this.getAttribute('aria-expanded') === 'true' || false;
+  this.setAttribute('aria-expanded', !expanded);
+
+  let menu = this.nextElementSibling;
+  menu.classList.toggle('open');
+});
